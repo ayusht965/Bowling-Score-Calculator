1ce09e6e6d8064d9f8c88d080319a78d079c8ada
Ayush Tyagi <ak7046243581@gmail.com>
2023-01-18 13:11:24 +0530

    fix: resolved the issue, feature working now

--- Bowling-Score-Counter/scoresAndBestPlayer.js
+++ Bowling-Score-Counter/scoresAndBestPlayer.js
@@ -1,50 +1,44 @@
-const prompt = require('prompt-sync')();
+
 
 const getTotalScore = (ScoreArray) => {
     let TotalScore = 0;
     let frameCount = 0;
+    let index = 0;
+    let length = ScoreArray.length;
     console.log(ScoreArray);
-    for (let index = 0; index < ScoreArray.length; index++) {
 
-        if (isNaN(ScoreArray[index])) throw new Error('Enter a valid input type');
-
-        if (ScoreArray[index] > 10) throw new Error('Enter valid input, should be less than or eqaul to 10');
-
-        if (ScoreArray[index] === 10) {
-            frameCount += 1;
-            if (frameCount > 10)
-                throw new Error('Invalid scores');
-            if (frameCount <= 10) {
-                if (index + 2 <= ScoreArray.length) {
-                    TotalScore = 10 + ScoreArray[index + 1] + ScoreArray[index + 2];
-                }
-
-            } else if (frameCount > 10) {
-                throw new Error('The input scores are more than 10 frames');
+    while (length > index) {
+        if (isNaN(ScoreArray[index]))
+            throw new Error('Invalid Input');
+        if (frameCount == 10) {
+            if (ScoreArray[index] == 10) {
+                TotalScore += 10 + ScoreArray[index + 1] + ScoreArray[index + 2];
+                index += 3;
+            } else if (ScoreArray[index + 1] + ScoreArray[index] == 10) {
+                TotalScore += ScoreArray[index + 1] + ScoreArray[index] + ScoreArray[index + 2];
+                index += 3;
             } else {
-                throw new Error('Invalid Input');
+                TotalScore = ScoreArray[index + 1] + ScoreArray[index];
             }
-        }
-        else if (ScoreArray[index] < 10) {
+        } else if (ScoreArray[index] == 10) {
+            TotalScore += 10 + ScoreArray[index + 1] + ScoreArray[index + 2];
+            index += 3;
             frameCount += 1;
-            if (frameCount > 10)
-                throw new Error('Invalid scores');
-            let frameScore = 0;
-            if (index + 1 <= ScoreArray.length) {
-                frameScore = ScoreArray[index] + ScoreArray[index + 1];
-                if (frameScore == 10) {
-                    if (index + 2 <= ScoreArray.length) {
-                        frameScore = ScoreArray[index + 2];
-                        index += 2;
-                    }
-                }
-            }
-            TotalScore += frameScore;
+        } else if (ScoreArray[index] + ScoreArray[index + 1] == 10) {
+            TotalScore += ScoreArray[index] + ScoreArray[index + 1] + ScoreArray[index + 2];
+            index += 2;
+            frameCount += 1;
+        } else {
+            TotalScore += ScoreArray[index] + ScoreArray[index + 1];
+            index += 2;
+            frameCount += 1;
         }
     }
+
+
     return TotalScore;
 
 };
-let ScoreArray = [];
-ScoreArray = prompt('Enter the scores');
-console.log(getTotalScore(ScoreArray));
+
+
+console.log(getTotalScore([3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]));
