d1e894f248e2c46bfb41198dfab4dc0c00085483
Ayush Tyagi <ak7046243581@gmail.com>
2023-01-18 18:40:15 +0530

    fix: resolve few edge cases

--- Bowling-Score-Counter/totalScore.js
+++ Bowling-Score-Counter/totalScore.js
@@ -1,19 +1,23 @@
 const getTotalScore = (ScoreArray) => {
     let TotalScore = 0;
-    let frameCount = 0;
     let index = 0;
     let length = ScoreArray.length;
+    if (length > 21) throw new Error('Invalid input size');
+    let frameCount = length == 21 ? 1 : 0;
     //console.log(ScoreArray);
 
     while (length > index) {
         if (isNaN(ScoreArray[index]))
             throw new Error('Invalid Input');
+        if (ScoreArray[index] > 10)
+            throw new Error('Invalid Input');
+
         if (frameCount == 10) {
             if (ScoreArray[index] == 10) {
                 TotalScore += 10 + ScoreArray[index + 1] + ScoreArray[index + 2];
                 index += 3;
             } else if (ScoreArray[index + 1] + ScoreArray[index] == 10) {
-                TotalScore += ScoreArray[index + 1] + ScoreArray[index] + ScoreArray[index + 2];
+                TotalScore += 10 + ScoreArray[index + 2];
                 index += 3;
             } else {
                 TotalScore = ScoreArray[index + 1] + ScoreArray[index];
@@ -23,7 +27,7 @@ const getTotalScore = (ScoreArray) => {
             index += 1;
             frameCount += 1;
         } else if (ScoreArray[index] + ScoreArray[index + 1] == 10) {
-            TotalScore += ScoreArray[index] + ScoreArray[index + 1] + ScoreArray[index + 2];
+            TotalScore += 10 + ScoreArray[index + 2];
             index += 2;
             frameCount += 1;
         } else {
@@ -32,10 +36,7 @@ const getTotalScore = (ScoreArray) => {
             frameCount += 1;
         }
     }
-
-
     return TotalScore;
-
 };
 
 // let scores = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
