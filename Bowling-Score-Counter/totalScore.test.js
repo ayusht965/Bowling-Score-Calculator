87d5f60ed8389e113d70aa10e6db553e65b4bf5f
Ayush Tyagi <ak7046243581@gmail.com>
2023-01-18 18:41:25 +0530

    chore: add new edge cases

--- Bowling-Score-Counter/totalScore.test.js
+++ Bowling-Score-Counter/totalScore.test.js
@@ -5,10 +5,22 @@ describe('Total score calculator test', () => {
         const scores1 = [3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
         expect(getTotalScore(scores1)).toBe(90);
     });
-    it('should give total score of 48 when score2 array is given', () => {
-        const scores2 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
-        expect(getTotalScore(scores2)).toBe(30);
+    it('should through an error if score is greater than 10', () => {
+        const scores2 = [3, 6, 11, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6];
+        expect(() => {
+            getTotalScore(scores2);
+        }).toThrow('Invalid Input');
     });
+    it('should give total score of 30 when score3 array is given', () => {
+        const scores3 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10];
+        expect(getTotalScore(scores3)).toBe(30);
+    });
+    it('should throw error if the input size is greater than 21', () => {
+        const scores4 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 10, 10, 10, 9];
+        expect(() => {
+            getTotalScore(scores4);
+        }).toThrow('Invalid input size');
+    })
     it('should throw an error if input is invalid', () => {
         expect(() => {
             getTotalScore([3, 6, 'abs', 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6, 3, 6]);
